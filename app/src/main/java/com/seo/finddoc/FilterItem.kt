54926aa0214717bb170e9e1c890473d4ba49cb70
wonkiSeo <dnjsrl4382@naver.com>
2022-11-27 08:32:26 +0900

    상단 검색바, 리사이클러 추가, gitignore 수정

---
+++ app/src/main/java/com/seo/finddoc/FilterItem.kt
@@ -0,0 +1,6 @@
+package com.seo.finddoc
+
+data class FilterItem(
+    val filterIcon: Int,
+    var filterName: String = ""
+)
--- app/src/main/java/com/seo/finddoc/MainActivity.kt
+++ app/src/main/java/com/seo/finddoc/MainActivity.kt
@@ -2,10 +2,24 @@ package com.seo.finddoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.seo.finddoc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val manager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        with(binding) {
+            filterRV.layoutManager = manager
+            filterRV.adapter = FilterRecyclerViewAdapter(filterData())
+        }
+    }
+    private fun filterData() = mutableListOf<FilterItem>().apply {
+        add(FilterItem(R.drawable.ic_baseline_local_hospital_24, "병원"))
     }
 }
