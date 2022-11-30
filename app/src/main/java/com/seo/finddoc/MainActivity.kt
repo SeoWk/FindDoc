926c796df65672b7249c65fd95293fe5cf0f0bc4
wonkiSeo <dnjsrl4382@naver.com>
2022-12-01 00:06:59 +0900

    네이버 지도 구현

--- app/src/main/java/com/seo/finddoc/MainActivity.kt
+++ app/src/main/java/com/seo/finddoc/MainActivity.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import com.seo.finddoc.adapter.FilterRecyclerViewAdapter
+import com.seo.finddoc.common.LOCATION_PERMISSION_REQUEST_CODE
+import com.seo.finddoc.common.PermissionCheck
 import com.seo.finddoc.common.toastMessage
 import com.seo.finddoc.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val manager = LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment?
+        mapFragment!!.getMapAsync(this@MainActivity)
         val filterAdapter = ArrayAdapter.createFromResource(
             this@MainActivity, R.array.filter_array_item, android.R.layout.simple_dropdown_item_1line
         )
@@ -58,6 +71,51 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onMapReady(p0: NaverMap) {
+        naverMap = p0
+
+        //지도 범위 제한 - 수정하기
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+        //초기 위치 지정 - 최근 검색위치나 현재 위치로 수정하기
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.472418245111065, 126.89592797777232))
+        naverMap.moveCamera(cameraUpdate)
+
+        //마커 표시
+
+        //현재 위치 잡기
+        val uiSettings = naverMap.uiSettings.apply {
+            isLocationButtonEnabled = true
+        }
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+    }
+    private lateinit var permissionCheck: PermissionCheck
+    override fun onResume() {
+        super.onResume()
+    }
+    private fun permissionCheck() {
+        permissionCheck = PermissionCheck(applicationContext,this@MainActivity)
+        if (!permissionCheck.currentAppCheckPermission()) {
+            permissionCheck.currentAppRequestPermission()
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!locationSource.isActivated){
+            naverMap.locationTrackingMode = LocationTrackingMode.None
+            return
+        }else{
+            naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        }
+    }
+
     private fun filterData() = mutableListOf<FilterItem>().apply {
         add(FilterItem(R.drawable.ic_baseline_local_hospital_24, "병원"))
     }
---
+++ app/src/main/java/com/seo/finddoc/common/PermissionCheck.kt
@@ -0,0 +1,43 @@
+package com.seo.finddoc.common
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+const val LOCATION_PERMISSION_REQUEST_CODE = 1004
+
+class PermissionCheck(private val context: Context, private val owner: Activity) {
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    private val permissionList = mutableListOf<String>()
+    fun currentAppCheckPermission(): Boolean{
+        for (permission in permissions) {
+            val result = ContextCompat.checkSelfPermission(context, permission)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(permission)
+            }
+        }
+        if (permissionList.isNotEmpty()) {
+            return false
+        }
+        return true
+    }
+    fun currentAppRequestPermission(){
+        ActivityCompat.requestPermissions(owner,permissionList.toTypedArray(),LOCATION_PERMISSION_REQUEST_CODE)
+    }
+    fun currentAppPermissionResult(requestCode : Int, grantResults : IntArray): Boolean {
+        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE && (grantResults.isNotEmpty())){
+            for(result in grantResults){
+                if(result == -1){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
