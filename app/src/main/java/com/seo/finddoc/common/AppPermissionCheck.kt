df15f0e51d6dd9e4aa8bd663f91bbb3b10428a1d
wonkiSeo <dnjsrl4382@naver.com>
2022-12-01 23:20:21 +0900

    네이저 맵 현위치 구현, bottomSheet 설정

--- app/src/main/java/com/seo/finddoc/common/AppPermissionCheck.kt
+++ app/src/main/java/com/seo/finddoc/common/AppPermissionCheck.kt
@@ -10,34 +10,5 @@ import androidx.core.content.ContextCompat
 const val LOCATION_PERMISSION_REQUEST_CODE = 1000
 
 class AppPermissionCheck(private val context: Context, private val owner: Activity) {
-    private val permissions = arrayOf(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
-    private val permissionList = mutableListOf<String>()
-    fun currentAppCheckPermission(): Boolean{
-        for (permission in permissions) {
-            val result = ContextCompat.checkSelfPermission(context, permission)
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                permissionList.add(permission)
-            }
-        }
-        if (permissionList.isNotEmpty()) {
-            return false
-        }
-        return true
-    }
-    fun currentAppRequestPermission(){
-        ActivityCompat.requestPermissions(owner,permissionList.toTypedArray(),LOCATION_PERMISSION_REQUEST_CODE)
-    }
-    fun currentAppPermissionResult(requestCode : Int, grantResults : IntArray): Boolean {
-        if(requestCode == LOCATION_PERMISSION_REQUEST_CODE && (grantResults.isNotEmpty())){
-            for(result in grantResults){
-                if(result == -1){
-                    return false
-                }
-            }
-        }
-        return true
-    }
+
 }
---
+++ app/src/main/java/com/seo/finddoc/fragment/BottomSheetFragment.kt
@@ -0,0 +1,10 @@
+package com.seo.finddoc.fragment
+
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
